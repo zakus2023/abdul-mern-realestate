c2fb3e2b679d776099adf0ff2d1a3116c09319fd
abdul <idbsch2012@gmail.com>
2024-03-11 17:54:13 -0400

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import mongoose from "mongoose"
-
-mongoose.connect("")
+import dotenv from "dotenv"
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected");
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express(); //creating the app
 
