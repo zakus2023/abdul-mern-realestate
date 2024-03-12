572d2ed795f1709fc87b1c4e121e181618467af5
abdul <idbsch2012@gmail.com>
2024-03-12 17:42:00 -0400

    created error middleware api and error function

--- api/Controllers/auth.controller.js
+++ api/Controllers/auth.controller.js
@@ -1,21 +1,19 @@
 import User from "../Models/user.models.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
+// import { errorHandler} from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  const hashedPassword = bcryptjs.hashSync(password,10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-  const newUser = new User({ username, email, password:hashedPassword });
+  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
     await newUser.save();
 
     res.status(200).json("User saved to mongodb");
-
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
-
-  
 };
--- api/index.js
+++ api/index.js
@@ -1,22 +1,38 @@
 import express from "express";
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import userRouter from './Routes/user.routes.js'
-import authRouter from './Routes/auth.route.js'
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./Routes/user.routes.js";
+import authRouter from "./Routes/auth.route.js";
 
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 const app = express(); //creating the app
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("App is listening on port: 3000"); //creating the listener port
-  });
+  console.log("App is listening on port: 3000"); //creating the listener port
+});
 
-  app.use('/api/user',userRouter)
-  app.use('/api/auth', authRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+//error handling middleware api
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+ 
