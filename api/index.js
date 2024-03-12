a5fee4e8685317990b3ba9ac160e4ddb3fdf9287
abdul <idbsch2012@gmail.com>
2024-03-11 23:31:50 -0400

    created a tset api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRouter from './Routes/user.routes.js'
+
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected");
@@ -13,3 +15,5 @@ const app = express(); //creating the app
 app.listen(3000, () => {
     console.log("App is listening on port: 3000"); //creating the listener port
   });
+
+  app.use('/api/user',userRouter)
