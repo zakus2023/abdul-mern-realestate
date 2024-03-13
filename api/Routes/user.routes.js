e9b94e5ed529ef5085e42cb893cdedbcd6fc5fed
abdul <idbsch2012@gmail.com>
2024-03-13 12:27:01 -0400

    created signin api route

--- api/Routes/user.routes.js
+++ api/Routes/user.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { test } from "../Controllers/user.controller.js";
+import { signin } from "../Controllers/auth.controller.js";
 
 const router = express.Router(); //creating the router
 
 router.get("/test",test);
+
 
 export default router;
