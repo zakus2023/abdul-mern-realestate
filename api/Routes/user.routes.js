1a51c7112ee9ceebe57feba406e26f148917b17c
abdul <idbsch2012@gmail.com>
2024-03-15 22:36:17 -0400

    added delete user functionality

--- api/Routes/user.routes.js
+++ api/Routes/user.routes.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { test, updateUser } from "../Controllers/user.controller.js";
+import {
+  deleteUser,
+  test,
+  updateUser,
+} from "../Controllers/user.controller.js";
 import { signin } from "../Controllers/auth.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -7,5 +11,6 @@ const router = express.Router(); //creating the router
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id",verifyToken, deleteUser);
 
 export default router;
--- client/src/Pages/Profiles.jsx
+++ client/src/Pages/Profiles.jsx
@@ -10,6 +10,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure
 } from "../redux/user/userSlice";
 
 import { app } from "../firebase";
@@ -93,6 +96,26 @@ export default function Profiles() {
     );
   };
 
+
+  const handleDeleteUser = async ()=>{
+    try {
+      dispatch(deleteUserStart())
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method: 'DELETE',
+      })
+      const data = await res.json();
+      if(data.success === false) {
+        dispatch(deleteUserFailure(data.message))
+        return;
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
+
+
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-bold text-center my-7">Profile</h1>
@@ -156,7 +179,7 @@ export default function Profiles() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 text-center">{error? error : ""}</p>
