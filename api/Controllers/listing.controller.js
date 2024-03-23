5728dc57a25e066ad892e39549b57b4a9967b50f
abdul <idbsch2012@gmail.com>
2024-03-22 21:19:26 -0400

    created api route for editing listing

--- api/Controllers/listing.controller.js
+++ api/Controllers/listing.controller.js
@@ -1,27 +1,47 @@
-import Listing from "../Models/listing.model.js"
+import Listing from "../Models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
-export const createListing = async (req, res, next)=>{
-    try {
-        const listing = await Listing.create(req.body);
-        return res.status(201).json(listing);
-    } catch (error) {
-        next(error)
-    }
-}
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteListing = async (req, res, next)=>{
-const listing = await Listing.findById(req.params.id) //checking whether the listing exist
-if(!listing){
-    return next(errorHandler(404, 'Listing not found'))
-}
-if(req.user.id !== listing.userRef){
-    return errorHandler(401, 'You can only delete your listing')
-}
-try {
-    await Listing.findByIdAndDelete(req.params.id)
-    res.status(200).json("Listing has been deleted")
-} catch (error) {
-    next(error)
-}
-}
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id); //checking whether the listing exist
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return errorHandler(401, "You can only delete your listing");
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(401, "You can only update your listing");
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/Routes/listing.route.js
+++ api/Routes/listing.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { createListing, deleteListing } from '../Controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing } from '../Controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router()
 
 router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
+router.post('/update/:id', verifyToken, updateListing)
 
 
 export default router;
