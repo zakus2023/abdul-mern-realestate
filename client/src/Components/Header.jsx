db15a870b0c5cc96d1e7800f37650176504b9344
abdul <idbsch2012@gmail.com>
2024-03-24 11:26:08 -0400

    updated header search form functionality

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import "../Components/Header.css";
 import { useSelector } from "react-redux";
+import { set } from "mongoose";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchterm] = useState("");
+  const navigate = useNavigate();
+
+  //sending the search term to the url
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  //getting the search term back to the input
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if(searchTermFromUrl){
+      setSearchterm(searchTermFromUrl)
+    }
+  }, [location.search]);
 
   return (
     <header className="bg-slate-200 shadow-md ">
@@ -14,13 +35,20 @@ export default function Header() {
         <Link to="/">
           <img src={logo} alt="" className="logo" />
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchterm(e.target.value)}
           />
-          <FaSearch className="text-slate-500 cursor-pointer" />
+          <button>
+            <FaSearch className="text-slate-500 cursor-pointer" />
+          </button>
         </form>
 
         <ul className="flex gap-7">
@@ -38,7 +66,11 @@ export default function Header() {
 
           <Link to="/profiles">
             {currentUser ? (
-              <img src={currentUser.avatar} alt="Profile" className="rounded-full h-9 w-9 object-cover" />
+              <img
+                src={currentUser.avatar}
+                alt="Profile"
+                className="rounded-full h-9 w-9 object-cover"
+              />
             ) : (
               <li className=" text-slate-700 hover:underline">Sign In</li>
             )}
