f6379a1d7a2cd95b6c4e95a12f8b8057e0861b1f
abdul <idbsch2012@gmail.com>
2024-03-25 11:45:36 -0400

    updated the sign in

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -2,13 +2,17 @@ import { data } from "autoprefixer";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 import OAuth from "../Components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
 
-  const {loading, error} = useSelector((state)=>state.user);
+  const { loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
 
@@ -22,48 +26,43 @@ export default function SignIn() {
   };
 
   const handleSubmit = async (e) => {
-
     e.preventDefault();
 
     try {
       dispatch(signInStart());
 
-    const res = await fetch(
-      "/api/auth/signin",
+      const res = await fetch(
+        "/api/auth/signin",
 
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      const data = await res.json();
+      console.log(data);
+
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
       }
-    );
 
-    const data = await res.json();
-    console.log(data);
+      dispatch(signInSuccess(data));
 
-    if (data.success === false) {
-      dispatch(signInFailure(data.message));
-      return;
-    }
-
-    dispatch(signInSuccess());
-
-    navigate('/');
-
+      navigate("/");
     } catch (error) {
       dispatch(signInFailure(error.message));
     }
-    
   };
- 
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold  my-7">Sign In</h1>
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-       
         <input
           type="email"
           placeholder="Email"
@@ -84,7 +83,7 @@ export default function SignIn() {
         >
           {loading ? "Loading . . ." : "Sign in"}
         </button>
-        <OAuth/>
+        <OAuth />
       </form>
       <div className="flex gap-2">
         <p>Do not have an account?</p>
